1beae7cc5df1a80d5e7db94947be6fc20088869d
sunhongxu <1808620172@qq.com>
2016-09-08 20:02:52 +0800

    改项目图标和名字等

---
+++ 36Kr/app/src/main/java/com/sunhongxu/a36kr/controler/fragment/NewsFragment.java
@@ -0,0 +1,23 @@
+package com.sunhongxu.a36kr.controler.fragment;
+
+import com.sunhongxu.a36kr.R;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class NewsFragment extends AbsBaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_news;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
